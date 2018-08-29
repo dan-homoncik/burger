f0c4ebe2ad1baa31eb8bf08953dfc2c886e91b05
DFloor Danny <37215615+dan-homoncik@users.noreply.github.com>
2018-08-29 11:11:29 -0400

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Quadruple Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("The Works");
+INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
